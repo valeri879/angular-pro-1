3bab68071ff8805227aa50c1085102a2d513979f
Val Do <valeri.kharitonashvili1@gmail.com>
2022-11-02 22:08:20 +0400

    lecture-5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './interfaces/user';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,12 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'angular-pro-1';
+
+  isActive: boolean = true;
+
+  users: User[] = [
+    { firstName: 'Bacho', lastName: 'Mindiashvili', gender: 'male', age: 20 },
+    { firstName: 'Val', lastName: 'Do', gender: 'male', age: 29 },
+    { firstName: 'Vika', lastName: 'Kharitonashvili', gender: 'famale', age: 18 },
+  ];
 }
