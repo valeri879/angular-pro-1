81a1927c426579debfd07517362c12f2d86b1606
Val Do <valeri.kharitonashvili1@gmail.com>
2022-11-21 17:32:15 +0400

    lecture 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-pro-1';
+
+  form!: FormGroup;
+
+/*   email: FormControl = new FormControl('', [Validators.required]);
+  password: FormControl = new FormControl(); */
+
+  constructor(
+    private _fb: FormBuilder
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.form = this._fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      rePassword: ['', [Validators.required]],
+      address: this._fb.array([])
+    });
+  }
+
+  get address(): FormArray {
+    return this.form.controls['address'] as FormArray
+  }
+
+  addAddress() {
+    const address = this._fb.group({
+      country: ['', [Validators.required]],
+      city: ['', [Validators.required]]
+    });
+
+    this.address.push(address);
+
+    console.log('form array values', this.address.value)
+  }
+
+  submit() {
+    console.log('form', this.form.controls);
+/*     console.log(`email`, this.email.invalid);
+    console.log(`password`, this.password.invalid); */
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
